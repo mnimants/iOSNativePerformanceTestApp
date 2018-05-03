c5d47112434fbc099ae755bbc3d3ab3894074838
mnimants <nimantsm@gmail.com>
2018-05-03 13:59:57 +0300

    Initial Commit

---
+++ KursaDarbs/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  KursaDarbs
+//
+//  Created by Marcis Nimants on 03/05/2018.
+//  Copyright © 2018 Marcis Nimants. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
